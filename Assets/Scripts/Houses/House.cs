29f7dac974e0b2f565b594bafacb795859d47fdd
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2019-01-25 23:28:02 +0100

    house wip

--- Assets/Scripts/Houses/House.cs
+++ Assets/Scripts/Houses/House.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class House : MonoBehaviour
 {
-
     Vector2 end_position;
+    Vector2 start_position;
+
+    float distance = 0.0f;
+
+    private void Update()
+    {
+        //float new_x = EasingFunctions.Linear(end_position.x - start_position.x, //Timer Partida, start_position.x, 300)
+        //float new_y = EasingFunctions.Linear(end_position.y - start_position.y, //Timer Partida, start_position.y, 300)
+        //transform.position = new Vector2(new_x, new_y);
+    }
 
     public void SetEndPosition(Vector2 _end_position)
     {
+        start_position = transform.position;
         end_position = _end_position;
     }
 }
