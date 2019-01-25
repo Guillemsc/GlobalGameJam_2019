7eee6f0856363d13ded55ba6641b42081eddabe0
Trodek <ibanmas@gmail.com>
2019-01-26 00:49:42 +0100

    player ui update score and events

---
+++ Assets/Scripts/Events/GameEvents/EventSubstractScore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSubstractScore : GameEvent {
+    public EventSubstractScore(PlayerStats player, int amount) : base(GameEventType.EVENT_SUBSTRACT_SCORE) {
+        this.player = player;
+        this.amount = amount;
+    }
+
+    public PlayerStats player = null;
+    public int amount = 0;
+
+}
