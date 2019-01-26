5db59d257d1bd57eb0cc4ffb8eb715cb70da8aa1
Guillemsc <gsunyercaldu@gmail.com>
2019-01-26 15:46:00 +0100

    Adding items

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ItemType
+{
+    ITEM_MAGNET,
+}
+
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected void Init(ItemType type)
     {
-        
+        this.type = type;
     }
 
-    // Update is called once per frame
-    void Update()
+    public ItemType Type()
     {
-        
+        return type;
     }
+
+    public void SetGrabbedBy(PlayerStats set)
+    {
+        grabbed_by = set;
+    }
+
+    public PlayerStats GetGrabbedBy()
+    {
+        return grabbed_by;
+    }
+
+    public bool GetIsGrabbed()
+    {
+        return grabbed_by != null;
+    }
+
+    private ItemType type = new ItemType();
+    private PlayerStats grabbed_by = null;
 }
