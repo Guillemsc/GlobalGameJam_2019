839e40a31baccffd4713520935c700c178e983e2
Trodek <ibanmas@gmail.com>
2019-01-26 09:58:39 +0100

    Quests Scene

---
+++ Assets/Scripts/Quests/QuestManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    [Tooltip("When each quest starts in seconds")]
+    public float[] quest_time = new float[0];
+
+    public List<Quest> quests = new List<Quest>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
