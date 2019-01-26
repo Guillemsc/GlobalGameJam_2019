a1e6f5a2cbba1bf0dd01f3428992ef2a7aeb68f6
Guillemsc <gsunyercaldu@gmail.com>
2019-01-26 15:32:57 +0100

    Adde base item class

---
+++ Assets/Scripts/Items/ItemManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : Singleton<ItemManager>
+{
+    ItemManager()
+    {
+        InitInstance(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    List<Item> GetItemsPrefabs()
+    {
+        return items_prefabs;
+    }
+
+    [SerializeField]
+    List<Item> items_prefabs = null;
+}
