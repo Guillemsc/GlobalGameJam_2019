bbcabf57cae3c4d7e93b842236320d427176b99f
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2019-01-26 17:34:55 +0100

    main menu ui going to controller menu

---
+++ Assets/Scripts/UI/UIPlayGameButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIPlayGameButton : MonoBehaviour
+{
+    [SerializeField]
+    GameObject controller_menu;
+
+
+    public void GoToControllerMenu()
+    {
+        GetComponent<AudioSource>().Play();
+        controller_menu.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    
+
+}
