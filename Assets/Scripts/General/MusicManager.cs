40d0ba084b2ce913005177fec403e21de19b9eb2
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2019-01-26 11:56:00 +0100

    added music to unity

---
+++ Assets/Scripts/General/MusicManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] AudioSource main_music_track;
+    [SerializeField] AudioClip clock_fx;
+    [SerializeField] AudioClip bomb_fx;
+    [SerializeField] AudioClip wrench_fx;
+    [SerializeField] AudioClip magnet_fx;
+    [SerializeField] AudioClip shot_fx;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            main_music_track.PlayOneShot(clock_fx);
+        }
+    }
+}
