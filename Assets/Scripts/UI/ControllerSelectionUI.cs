1d99bd41c09541a22003a3765a76001eef050195
Guillemsc <gsunyercaldu@gmail.com>
2019-01-26 23:58:48 +0100

    Worrking on color selector

---
+++ Assets/Scripts/UI/ControllerSelectionUI.cs
@@ -0,0 +1,278 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerSelectionUI : Singleton<ControllerSelectionUI>
+{
+    ControllerSelectionUI()
+    {
+        InitInstance(this);
+    }
+
+    private void Awake()
+    {
+        InitQueueEvent();
+    }
+
+    private void Update()
+    {
+        if (selecting_gamepads)
+        {
+            UpdateControllersInput();
+        }
+
+        CheckReady();
+    }
+
+    private void InitQueueEvent()
+    {
+        queue_context = QueueEventManager.Instance.CreateContext();
+    }
+
+    public void SetSelectingGamepads(bool set)
+    {
+        selecting_gamepads = set;
+
+        gameObject.SetActive(set);
+    }
+
+    private void UpdateControllersInput()
+    {
+        int players_count = PlayersManager.Instance.GetPlayersCount();
+
+        for(int i = 0; i < players_count; ++i)
+        {
+            Player curr_player = PlayersManager.Instance.GetPlayerByIndex(i);
+
+            bool already_placed = false;
+
+            if(curr_player == red_player)
+            {
+                already_placed = true;
+            }
+            else if (curr_player == yellow_player)
+            {
+                already_placed = true;
+            }
+            else if (curr_player == blue_player)
+            {
+                already_placed = true;
+            }
+
+            if(already_placed)
+            {
+                if(curr_player.GetKeyB(GamepadGetButtonType.DOWN))
+                {
+                    GameObject go_to_move = null;
+                    GameObject place_to_move = null;
+
+                    switch(i)
+                    {
+                        case 0:
+                            {
+                                go_to_move = controller_1;
+                                place_to_move = controller_1_pos;
+                                break;
+                            }
+                        case 1:
+                            {
+                                go_to_move = controller_2;
+                                place_to_move = controller_2_pos;
+                                break;
+                            }
+                        case 2:
+                            {
+                                go_to_move = controller_3;
+                                place_to_move = controller_3_pos;
+                                break;
+                            }
+                    }
+
+                    queue_context.PushEvent(new
+                    QueueEventPosition(go_to_move,
+                    go_to_move.gameObject.transform.position, place_to_move.gameObject.transform.position,
+                    0.2f, EasingFunctionsType.EXPO_IN));
+
+                    if (curr_player == red_player)
+                    {
+                        red_player = null;
+                        red_ready = false;
+
+                        red_go_ready.SetActive(false);
+                    }
+                    else if (curr_player == yellow_player)
+                    {
+                        yellow_player = null;
+                        yellow_ready = false;
+
+                        yellow_go_ready.SetActive(false);
+                    }
+                    else if (curr_player == blue_player)
+                    {
+                        blue_player = null;
+                        blue_ready = false;
+
+                        blue_go_ready.SetActive(false);
+                    }
+                }
+                else if(curr_player.GetKeyStart(GamepadGetButtonType.DOWN))
+                {
+                    if (curr_player == red_player)
+                    {
+                        red_ready = true;
+
+                        red_go_ready.SetActive(true);
+                    }
+                    else if (curr_player == yellow_player)
+                    {
+                        yellow_ready = true;
+
+                        yellow_go_ready.SetActive(true);
+                    }
+                    else if (curr_player == blue_player)
+                    {
+                        blue_ready = true;
+
+                        blue_go_ready.SetActive(true);
+                    }
+                }
+            }
+            else
+            {
+                GameObject go_to_move = null;
+                switch (i)
+                {
+                    case 0:
+                        {
+                            go_to_move = controller_1;
+                            break;
+                        }
+                    case 1:
+                        {
+                            go_to_move = controller_2;
+                            break;
+                        }
+                    case 2:
+                        {
+                            go_to_move = controller_3;
+                            break;
+                        }
+                }
+
+                if (curr_player.DPadHorizontal() < 0)
+                {
+                    if (red_player == null)
+                    {
+                        red_player = curr_player;
+
+                        queue_context.PushEvent(new
+                        QueueEventPosition(go_to_move,
+                        go_to_move.transform.position, red_pos.gameObject.transform.position,
+                        0.2f, EasingFunctionsType.EXPO_IN));
+                    }
+                }
+
+                if (curr_player.DPadVertical() > 0)
+                {
+                    if (yellow_player == null)
+                    {
+                        yellow_player = curr_player;
+
+                        queue_context.PushEvent(new
+                        QueueEventPosition(go_to_move,
+                        go_to_move.transform.position, yellow_pos.gameObject.transform.position,
+                        0.2f, EasingFunctionsType.EXPO_IN));
+                    }
+                }
+
+                if (curr_player.DPadHorizontal() > 0)
+                {
+                    if (blue_player == null)
+                    {
+                        blue_player = curr_player;
+
+                        queue_context.PushEvent(new
+                        QueueEventPosition(go_to_move,
+                        go_to_move.transform.position, blue_pos.gameObject.transform.position,
+                        0.2f, EasingFunctionsType.EXPO_IN));
+                    }
+                }
+            }
+        }
+    }
+
+    private void CheckReady()
+    {
+        if(red_ready && blue_ready && yellow_ready)
+        {
+            selecting_gamepads = false;
+        }
+    }
+
+    private void OnController1Stoped(QueueEvent ev)
+    {
+        controller_1_moving = false;
+    }
+
+    private void OnController2Stoped(QueueEvent ev)
+    {
+        controller_2_moving = false;
+    }
+
+    private void OnController3Stoped(QueueEvent ev)
+    {
+        controller_3_moving = false;
+    }
+
+    private QueueEventContext queue_context = null;
+
+    private bool selecting_gamepads = false;
+
+    [SerializeField]
+    private GameObject red_pos = null;
+
+    [SerializeField]
+    private GameObject red_go_ready = null;
+
+    [SerializeField]
+    private GameObject yellow_pos = null;
+
+    [SerializeField]
+    private GameObject yellow_go_ready = null;
+
+    [SerializeField]
+    private GameObject blue_pos = null;
+
+    [SerializeField]
+    private GameObject blue_go_ready = null;
+
+    [SerializeField]
+    private GameObject controller_1 = null;
+    private bool controller_1_moving = false;
+
+    [SerializeField]
+    private GameObject controller_1_pos = null;
+
+    [SerializeField]
+    private GameObject controller_2 = null;
+    private bool controller_2_moving = false;
+
+    [SerializeField]
+    private GameObject controller_2_pos = null;
+
+    [SerializeField]
+    private GameObject controller_3 = null;
+    private bool controller_3_moving = false;
+
+    [SerializeField]
+    private GameObject controller_3_pos = null;
+
+    private Player red_player = null;
+    private Player yellow_player = null;
+    private Player blue_player = null;
+
+    private bool red_ready = false;
+    private bool yellow_ready = false;
+    private bool blue_ready = false;
+}
