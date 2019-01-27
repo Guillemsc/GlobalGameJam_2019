97b5c77d191dd7be439553d9684345352ec8c0bb
Trodek <ibanmas@gmail.com>
2019-01-27 13:22:32 +0100

    enduu

--- Assets/Ranking.cs
+++ Assets/Ranking.cs
@@ -15,32 +15,89 @@ public class Ranking : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        List<House> house_ranking = HouseManager.Instance.GetHousesOrderedByPoints();
 
-        for(int i = 0; i< house_ranking.Count; ++i) 
-        {
-            switch (house_ranking[i].GetPlayerInstance().GetPlayer().GetPlayerColour()) {
-                case PlayerColour.RED:
-                    ranking_p1.text = (i + 1).ToString();
-                    points_p1.text = house_ranking[i].GetPoints().ToString("D5");
+        EventManager.Instance.Suscribe(GameEventType.EVENT_MATCH_FINISH, OnEvent);
+        EventManager.Instance.Suscribe(GameEventType.EVENT_MAP_LOAD, OnEvent);
+        transform.parent.gameObject.SetActive(false);
+        
+    }
+
+    public void OnEvent(GameEvent ev) 
+    {
+        switch (ev.Type()) {
+            case GameEventType.EVENT_NULL:
+                break;
+            case GameEventType.EVENT_PLAYER_ADDED:
+                break;
+            case GameEventType.EVENT_PLAYER_REMOVED:
+                break;
+            case GameEventType.EVENT_PLAYER_SPAWNED:
+                break;
+            case GameEventType.EVENT_PLAYER_DESPAWNED:
+                break;
+            case GameEventType.EVENT_GAMEPADS_CHANGED:
+                break;
+            case GameEventType.EVENT_GAMEPAD_ADDED:
+                break;
+            case GameEventType.EVENT_GAMEPAD_REMOVED:
+                break;
+            case GameEventType.EVENT_SET_SCORE:
+                break;
+            case GameEventType.EVENT_SUBSTRACT_SCORE:
+                break;
+            case GameEventType.EVENT_START_QUEST:
+                break;
+            case GameEventType.EVENT_END_QUEST:
+                break;
+            case GameEventType.EVENT_HOUSES_SPAWNED:
+                break;
+            case GameEventType.EVENT_MAP_LOAD:
+                transform.parent.gameObject.SetActive(false);
+                break;
+            case GameEventType.EVENT_MAP_UNLOAD:
+                break;
+            case GameEventType.EVENT_MATCH_START:
+                break;
+            case GameEventType.EVENT_MATCH_FINISH: 
+                {
+                    transform.parent.gameObject.SetActive(true);
+
+                    List<House> house_ranking = HouseManager.Instance.GetHousesOrderedByPoints();
+
+                    for (int i = 0; i < house_ranking.Count; ++i) {
+                        switch (house_ranking[i].GetPlayerInstance().GetPlayer().GetPlayerColour()) {
+                            case PlayerColour.RED:
+                                ranking_p1.text = (i + 1).ToString();
+                                points_p1.text = house_ranking[i].GetPoints().ToString("D5");
+                                break;
+                            case PlayerColour.BLUE:
+                                ranking_p3.text = (i + 1).ToString();
+                                points_p2.text = house_ranking[i].GetPoints().ToString("D5");
+                                break;
+                            case PlayerColour.YELLOW:
+                                ranking_p2.text = (i + 1).ToString();
+                                points_p3.text = house_ranking[i].GetPoints().ToString("D5");
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
                     break;
-                case PlayerColour.BLUE:
-                    ranking_p3.text = (i + 1).ToString();
-                    points_p2.text = house_ranking[i].GetPoints().ToString("D5");
-                    break;
-                case PlayerColour.YELLOW:
-                    ranking_p2.text = (i + 1).ToString();
-                    points_p3.text = house_ranking[i].GetPoints().ToString("D5");
-                    break;
-                default:
-                    break;
-            }
+                }
+            case GameEventType.EVENT_ITEM_GRABBED:
+                break;
+            case GameEventType.EVENT_ITEM_DROPPED:
+                break;
+            case GameEventType.EVENT_ITEM_ENTERS_HOUSE:
+                break;
+            case GameEventType.EVENT_ITEM_LEAVES_HOUSE:
+                break;
+            case GameEventType.EVENT_BULLET_HITS_PLAYER:
+                break;
+            default:
+                break;
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
